9578dde338d4cb03a7649fa49e8f3c1d86e38047
Sam Schlinkert <sschlinkert@gmail.com>
2023-01-19 01:45:17 -0500

    adds function that isolates HTML body tag from scrape websites before making a word list

--- src/website_scraper.rs
+++ src/website_scraper.rs
@@ -1,34 +1,44 @@
 use regex::Regex;
+use scraper::{Html, Selector};
 
 pub fn download_page(page_url: &str) -> Result<String, ureq::Error> {
-  let body: String = ureq::get(page_url)
-    .call()?
-    .into_string()?;
+    let body: String = ureq::get(page_url).call()?.into_string()?;
 
-  Ok(body)
+    Ok(body)
+}
+
+/// Remove everything from page_body except the
+/// HTML within the <body></body> HTML tags.
+fn just_body_html_content(all_html: &str) -> String {
+    let fragment = Html::parse_document(all_html);
+    let selector = Selector::parse("body").unwrap();
+    let body = fragment.select(&selector).next().unwrap();
+    body.text().collect::<Vec<&str>>().join(" ")
 }
 
 pub fn extract_words(page_body: &str) -> Vec<String> {
-  let mut words: Vec<String> = vec![ ];
-  
-  let html_tag = Regex::new("<[^>]*>").unwrap();
-  // let body_wo_scripts = remove_script_tags(&page_body);
+    let page_body = just_body_html_content(page_body);
 
-  // println!("{body_wo_scripts}");
+    let mut words: Vec<String> = vec![];
 
-  for line in html_tag
-    .replace_all(page_body, "")
-    .split(' ')
-    .collect::<Vec<&str>>() {
-    let trimmed_line = line.trim();
-    if !trimmed_line.is_empty() {
-      for word in trimmed_line.split(' ').collect::<Vec<&str>>() {
-        let w = word.trim().to_owned();
-        if words.contains(&w) { continue } 
-        words.push(w)
-      }
+    let html_tag = Regex::new("<[^>]*>").unwrap();
+
+    for line in html_tag
+        .replace_all(&page_body, "")
+        .split(' ')
+        .collect::<Vec<&str>>()
+    {
+        let trimmed_line = line.trim();
+        if !trimmed_line.is_empty() {
+            for word in trimmed_line.split(' ').collect::<Vec<&str>>() {
+                let w = word.trim().to_owned();
+                if words.contains(&w) {
+                    continue;
+                }
+                words.push(w)
+            }
+        }
     }
-  }
 
-  words
+    words
 }
