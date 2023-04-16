e11b9de8ca441f0421216013af24f62f5634d318
Sam Schlinkert <sschlinkert@gmail.com>
2023-04-16 19:07:18 -0400

    runs rustfmt of tests and website_scraper files

--- src/website_scraper.rs
+++ src/website_scraper.rs
@@ -1,34 +1,35 @@
 use regex::Regex;
 
 pub fn download_page(page_url: &str) -> Result<String, ureq::Error> {
-  let body: String = ureq::get(page_url)
-    .call()?
-    .into_string()?;
+    let body: String = ureq::get(page_url).call()?.into_string()?;
 
-  Ok(body)
+    Ok(body)
 }
 
 pub fn extract_words(page_body: &str) -> Vec<String> {
-  let mut words: Vec<String> = vec![ ];
-  
-  let html_tag = Regex::new("<[^>]*>").unwrap();
-  // let body_wo_scripts = remove_script_tags(&page_body);
+    let mut words: Vec<String> = vec![];
 
-  // println!("{body_wo_scripts}");
+    let html_tag = Regex::new("<[^>]*>").unwrap();
+    // let body_wo_scripts = remove_script_tags(&page_body);
 
-  for line in html_tag
-    .replace_all(page_body, "")
-    .split(' ')
-    .collect::<Vec<&str>>() {
-    let trimmed_line = line.trim();
-    if !trimmed_line.is_empty() {
-      for word in trimmed_line.split(' ').collect::<Vec<&str>>() {
-        let w = word.trim().to_owned();
-        if words.contains(&w) { continue } 
-        words.push(w)
-      }
+    // println!("{body_wo_scripts}");
+
+    for line in html_tag
+        .replace_all(page_body, "")
+        .split(' ')
+        .collect::<Vec<&str>>()
+    {
+        let trimmed_line = line.trim();
+        if !trimmed_line.is_empty() {
+            for word in trimmed_line.split(' ').collect::<Vec<&str>>() {
+                let w = word.trim().to_owned();
+                if words.contains(&w) {
+                    continue;
+                }
+                words.push(w)
+            }
+        }
     }
-  }
 
-  words
+    words
 }
