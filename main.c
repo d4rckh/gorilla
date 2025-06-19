512fa76c261696df5d6e7782d9a211f0b2d21e08
andrei <35298550+d4rckh@users.noreply.github.com>
2025-06-19 11:29:49 +0300

    clean

--- main.c
+++ main.c
@@ -2,12 +2,12 @@
 #include "threads.h"
 
 Token tokens[3] = {
-    {0, 1, TEXT,   {.text = "administrator"}},
-    {0, 10, COUNT, {.start = 1}},
-    {0, 26, LETTER, {.start = 'a'}}
-};
+    {0,     1,      TEXT,       {.text = "administrator"}},
+    {0,     10,     COUNT,      {.start = 1}},
+    {0,     26,     LETTER,     {.start = 'a'}}};
 
-int main() {
-    distribute_threads(tokens, 3,3);
+int main()
+{
+    distribute_threads(tokens, 3, 3);
     return 0;
 }
